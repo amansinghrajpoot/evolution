ff45843cd4232fe45e4dc0e9a305ad2cee7e8a3b
Aman Rajput <v-amrajput@expediagroup.com>
2023-09-22 10:40:36 +0530

    improved GUI and added repaint capability

--- src/main/java/com/me/projects/EvolutionApp.kt
+++ src/main/java/com/me/projects/EvolutionApp.kt
@@ -3,9 +3,17 @@ package com.me.projects
 import com.me.projects.game.gui.GUI
 
 fun main(args: Array<String>) {
+    val gui = GUI()
     val pointsList = ArrayList<Pair<Int, Int>>()
     pointsList.add(Pair<Int, Int>(300, 300))
     pointsList.add(Pair<Int, Int>(500, 500))
     pointsList.add(Pair<Int, Int>(600, 400))
-    GUI().draw(pointsList)
+    gui.draw(pointsList)
+
+    Thread.sleep(2000)
+
+    pointsList.add(Pair<Int, Int>(100, 300))
+    pointsList.add(Pair<Int, Int>(100, 500))
+    pointsList.add(Pair<Int, Int>(300, 400))
+    gui.draw(pointsList)
 }
--- src/main/java/com/me/projects/game/gui/GUI.java
+++ src/main/java/com/me/projects/game/gui/GUI.java
@@ -6,20 +6,25 @@ import java.awt.*;
 import java.util.ArrayList;
 
 public class GUI {
-    public void draw(ArrayList<Pair<Integer, Integer>> pointsList) {
 
-        JFrame frame = new JFrame("Evolution (The zero player game)");
+    private JFrame frame;
+    private PointPanel pointPanel;
+
+    public GUI() {
+        frame = new JFrame("Evolution (The zero-player game)");
         frame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setResizable(false);
 
-        PointPanel pointPanel = new PointPanel();
+        pointPanel = new PointPanel();
         pointPanel.setBackground(Color.decode(BACKGROUND_COLOR));
-
-        pointPanel.setPointsList(pointsList);
 
         frame.add(pointPanel);
         frame.setVisible(true);
+    }
+
+    public void draw(ArrayList<Pair<Integer, Integer>> pointsList) {
+        pointPanel.setPointsList(pointsList);
     }
 
     class PointPanel extends JPanel {
@@ -27,16 +32,13 @@ public class GUI {
 
         public void setPointsList(ArrayList<Pair<Integer, Integer>> pointsList) {
             this.pointsList = pointsList;
+            this.repaint();
         }
 
         @Override
         protected void paintComponent(Graphics g) {
             super.paintComponent(g);
-
-            // Set the color for the points (e.g., green)
             g.setColor(Color.GREEN);
-
-            // Paint each point in the list
             if (pointsList != null) {
                 for (Pair<Integer, Integer> point : pointsList) {
                     int x = point.getFirst();
