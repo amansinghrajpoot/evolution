c1842e1d06533a231a39edb9c474240200425cab
Aman Rajput <v-amrajput@expediagroup.com>
2023-09-22 22:19:13 +0530

    change colors and added bottom panel for information

--- src/main/java/com/me/projects/EvolutionApp.kt
+++ src/main/java/com/me/projects/EvolutionApp.kt
@@ -9,16 +9,16 @@ fun main() {
     val cell = Cell(intArrayOf(500, 350), doubleArrayOf(0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0))
     val cells = arrayListOf(cell)
 
-    paintCellsOnGUI(cells)
+    paintCellsOnGUI(cells, "Generation: 1")
     for (i in 1..700) {
         makeCellsMove(cells)
         Thread.sleep(50)
         if (i % 10 == 0) {
-            paintCellsOnGUI(cells)
+            paintCellsOnGUI(cells, "Generation: 1")
         }
         if (i % 200 == 0) {
             mutateGenes(cells)
         }
     }
-    paintCellsOnGUI(cells)
+    paintCellsOnGUI(cells, "Generation: 1")
 }
